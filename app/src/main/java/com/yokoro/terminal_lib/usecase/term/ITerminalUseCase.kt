c983ef6d999bd61fc0b121d5db16d16d34955ff1
yokoro13 <i14yokoro@tokuyama.kosen-ac.jp>
2021-03-15 16:32:24 +0900

    Add TerminalのUseCase

--- app/src/main/java/com/yokoro/terminal_lib/usecase/term/ITerminalUseCase.kt
+++ app/src/main/java/com/yokoro/terminal_lib/usecase/term/ITerminalUseCase.kt
@@ -1,4 +1,23 @@
 package com.yokoro.terminal_lib.usecase.term
 
+import com.yokoro.terminal_lib.entity.Cursor
+import com.yokoro.terminal_lib.entity.ScreenSize
+
 interface ITerminalUseCase {
+
+    suspend fun addNewRow(warp: Boolean)
+
+    suspend fun createTerminalBuffer()
+
+    suspend fun resize()
+
+    suspend fun getScreenSize(): ScreenSize
+
+    suspend fun getTopRow(): Int
+
+    suspend fun setTopRow(n: Int)
+
+    suspend fun inputText(cursor: Cursor, text: Char)
+
+    suspend fun inputColor(cursor: Cursor, color: Int)
 }
--- app/src/main/java/com/yokoro/terminal_lib/usecase/term/TerminalUseCase.kt
+++ app/src/main/java/com/yokoro/terminal_lib/usecase/term/TerminalUseCase.kt
@@ -1,13 +1,47 @@
 package com.yokoro.terminal_lib.usecase.term
 
+import com.yokoro.terminal_lib.entity.Cursor
+import com.yokoro.terminal_lib.entity.ScreenSize
 import com.yokoro.terminal_lib.usecase.term.*
 
 class TerminalUseCase (
+    private val addNewRow: AddNewRow,
     private val createTerminalBuffer: CreateTerminalBuffer,
-    private val getTextBuffer: GetTextBuffer,
     private val resize: Resize,
     private val setColor: SetColor,
     private val setText: SetText,
     private val getScreenSize: GetScreenSize,
-    ) {
+    ): ITerminalUseCase {
+
+    override suspend fun addNewRow(warp: Boolean) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun createTerminalBuffer() {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun resize() {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getScreenSize(): ScreenSize {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getTopRow(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun setTopRow(n: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun inputText(cursor: Cursor, text: Char) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun inputColor(cursor: Cursor, color: Int) {
+        TODO("Not yet implemented")
+    }
 }
