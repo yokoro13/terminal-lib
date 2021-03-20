9ccb2fac05cc3b9b46d596c852c75e3b26f6ae69
yokoro13 <i14yokoro@tokuyama.kosen-ac.jp>
2021-03-20 18:58:34 +0900

    Delete unnecessary listeners

--- app/src/main/java/com/yokoro/terminal_lib/view/GestureListener.kt
+++
@@ -1,8 +0,0 @@
-package com.yokoro.terminal_lib.view
-
-import java.util.*
-
-interface GestureListener: EventListener{
-    fun onDown()
-    fun onMove()
-}
--- app/src/main/java/com/yokoro/terminal_lib/view/InputListener.kt
+++
@@ -1,7 +0,0 @@
-package com.yokoro.terminal_lib.view
-
-import java.util.*
-
-interface InputListener: EventListener {
-    fun onKey(text: Char)
-}
