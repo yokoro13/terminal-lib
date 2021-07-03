08a38538f3a37ccf67c1a373c323f2fad423758d
yokoro13 <yokoro.kaito.yi3@is.naist.jp>
2021-07-04 02:27:40 +0900

    Add UseCaseとControllerを分離させるinterface

--- terminal-lib/src/commonMain/kotlin/usecase/UseCaseInputPort.kt
+++ terminal-lib/src/commonMain/kotlin/usecase/UseCaseInputPort.kt
@@ -1,4 +1,29 @@
 package usecase
 
-class UseCaseInputPort {
+import entity.Cursor
+import entity.ScreenSize
+import entity.TerminalArray
+import entity.TerminalChar
+
+/**
+ * FIXME 命名が不適切
+ */
+
+interface UseCaseInputPort {
+    fun setTerminalChar(x: Int, y: Int, terminalChar: TerminalChar)
+    fun setText(x: Int, y: Int, text: Char)
+    fun setCursor(cursor: Cursor)
+    fun setScreenSize(screenSize: ScreenSize)
+    fun setForeColor(x: Int, y: Int, foreColor: Int)
+    fun setBackColor(x: Int, y: Int, backColor: Int)
+
+    fun getText(x: Int, y: Int): TerminalChar
+    fun getScreenText(): ArrayList<TerminalArray>
+    fun getCursor(): Cursor
+    fun getScreenSize(): ScreenSize
+
+    fun scrollDown(n: Int)
+    fun scrollUp(n: Int)
+
+    fun writeCharOrRunEscSeq(text: String)
 }
--- terminal-lib/src/commonMain/kotlin/usecase/UseCaseOutputPort.kt
+++ terminal-lib/src/commonMain/kotlin/usecase/UseCaseOutputPort.kt
@@ -1,4 +1,17 @@
 package usecase
 
+import entity.Cursor
+import entity.ScreenSize
+import entity.TerminalArray
+import entity.TerminalChar
+
+/**
+ * FIXME 命名が不適切
+ */
+
 interface UseCaseOutputPort {
+    fun getText(terminalChar: TerminalChar): TerminalChar
+    fun getScreenText(screenText: ArrayList<TerminalArray>): ArrayList<TerminalArray>
+    fun getCursor(cursor: Cursor): Cursor
+    fun getScreenSize(screenSize: ScreenSize): ScreenSize
 }
